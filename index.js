11cc525d4c5dca61fbaab1b8fc196170c1c6aaf3
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-17 14:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much.";
 
 kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(kitkat){
-  kittens.push(kitkat);
+function destructivelyAppendKitten(name){
+  kittens.push(name)
 }
