26e9cba78e8cf511321926da064de63c096c6d87
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-17 14:15:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 kittens = ["Milo", "Otis", "Garfield"]
 
-var func = function destructivelyAppendKitten(name){
+var destructivelyAppendKitten = function(name){
   kittens.push(name)
 }
