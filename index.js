8b04e4ac068531f080994c8807bafdccec0a2d33
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-17 14:37:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,22 +15,22 @@ function destructivelyRemoveLastKitten(){
 }
 
 function destructivelyRemoveFirstKitten(){
-
+  kittens.shift;
 }
 
 function appendKitten(name) {
-
+  return [...kittens, name]
 }
 
 function prependKitten(name){
-
+  return [name, ...kittens]
 }
 
 
 function removeLastKitten(){
-
+  kittens.slice(0,a.length-1)
 }
 
 function removeFirstKitten(){
-
+  kittens.slice(1)
 }
