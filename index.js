bdebbb4a6d920e3d438a1e40365b023aeadb8fe7
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-17 14:13:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const app = "I don't do much.";
+const app = "I don't do much."
 
-kittens = ["Milo", "Otis", "Garfield"];
+kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
   kittens.push(name)
