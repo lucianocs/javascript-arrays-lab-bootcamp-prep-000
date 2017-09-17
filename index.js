21f73ba5a06f2c275da56deed30d3f6b946442fa
Luciano Coelho de Souza <luciano.coelhodesouza@gmail.com>
2017-09-17 14:26:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 
 var kittens = ["Milo", "Otis", "Garfield"]
+
+
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  }
+
+function destructivelyPrependKitten(name){
+  
+  }
+
+  function destructivelyRemoveLastKitten(){
+    
+  }
+
+  function destructivelyRemoveFirstKitten(){
+  
+  }
+
+  function appendKitten(name) {
+
+  }
+
+  function prependKitten(name){
+    
+  }
+
+  
+  function removeLastKitten(){
+
+  }
+
+  function removeFirstKitten(){
+    
+  }
